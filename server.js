a1c08d9f5c1239adf3a124ebb3acbe92ad0c72e8
Rodrigo Temporim <juniortemporim@gmail.com>
2021-10-27 00:23:31 -0300

    port

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 import express, { static } from 'express'
 const app = express()
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 3000, () => { console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env); });
  
 app.use(static('build'))
 app.listen(PORT, () => console.log('Servidor Front em execução!'))
